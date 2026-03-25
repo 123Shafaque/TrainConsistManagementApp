9ac0ea8723dc70c658722c8af7ebaab8c49ef890
123Shafaque <sa3980@srmist.edu.in>
2026-03-25 09:44:18 +0530

    UC1 welcome message

--- src/TrainApp.java
+++ src/TrainApp.java
@@ -1,8 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class UseCase1TrainConsistMgmt {
-
+public class TrainApp {
     public static void main(String[] args) {
 
         System.out.println("======================================");
